dee4b585bc4aebda991646f38e1f8b84e852c7c1
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-01-31 21:38:14 -0300

    configurações iniciais

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export default () => {
+  return (
+    <div className="appWindow">
+      <div className="sidebar"></div>
+
+      <div className="contentArea"></div>
+    </div>
+  )
+}
