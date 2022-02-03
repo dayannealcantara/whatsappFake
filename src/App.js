620a552f3593f7b21028f14dcb8dff5b45b9b5a2
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-02-02 21:35:37 -0300

    busca de nova conversa

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React from "react";
 import "./App.css";
-import ImagemUsuario from"./imagens/usuario.png"
+import ImagemUsuario from "./imagens/usuario.png";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
-import MoreVertIcon from"@material-ui/icons/MoreVert"
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from"@material-ui/icons/Search"
 
-export default () => {
+const App = () => {
   return (
     <div className="appWindow">
       <div className="sidebar">
         <header>
           <img src={ImagemUsuario} className="imgUsuario" />
           <div className="header-btn">
-          <div className="button">
-            <DonutLargeIcon style={{color:'#919191'}} />
+            <div className="button">
+              <DonutLargeIcon style={{ color: "#919191" }} />
             </div>
             <div className="button">
-            <ChatIcon style={{color:'#919191'}} />
+              <ChatIcon style={{ color: "#919191" }} />
             </div>
             <div className="button">
-            <MoreVertIcon style={{color:'#919191'}} />
+              <MoreVertIcon style={{ color: "#919191" }} />
             </div>
           </div>
         </header>
-        <div className="busca"></div>
+        <div className="busca">
+          <div className="buscaInput">
+            <SearchIcon   fontSize="small" style={{ color: "#919191" }} />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa"/>
+          </div>
+
+
+        </div>
+
         <div className="chatList"></div>
       </div>
 
@@ -31,3 +40,5 @@ export default () => {
     </div>
   );
 };
+
+export default App;
